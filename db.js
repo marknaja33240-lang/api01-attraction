d926a72ce6e36a59278fe85b4c3951818c85f5d4
marknaja33240-lang <marknaja33240@gmail.com>
2026-03-28 17:17:10 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 4000,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 4000, 
+    ssl: {
+        rejectUnauthorized: true // บังคับใช้ SSL สำหรับ TiDB Cloud
+    },
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
-    // เปิดใช้งาน SSL สำหรับ TiDB Cloud ตามที่กำหนดใน .env
-    ssl: process.env.DB_SSL === 'true' ? { minVersion: 'TLSv1.2' } : null
+    queueLimit: 0
 });
 
-module.exports = pool;
+module.exports = pool.promise();
